b616540406bc26242f0ce00973a64063b3a62cf2
Hassnain khan <naintechnical1@gmail.com>
2024-12-12 19:42:49 +0500

    Day 4

---
+++ Day4.py
@@ -0,0 +1,7 @@
+# Day 4
+#For Accessing List Items 
+New_list = list(("Orange", "Berry", "Mango"))
+print(New_list)
+
+print(New_list[1])  #indexing starts from 0
+print(New_list[-1]) #Negative indexing means start from the end (-1 refers to the last item,)

e2c40d3f249cde674dc50169f6eae6e1a56f467a
Hassnain khan <naintechnical1@gmail.com>
2024-12-13 21:53:54 +0500

    Day 5

---
+++ Day5.py
@@ -0,0 +1,11 @@
+#Range of List
+ 
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+
+print (thislist[:]) #For printing every item
+
+print (thislist[2:5]) #The search will start at index 2 (included) and end at index 5 (not included).
+
+print(thislist[:4]) #it will print from first item to the 4th
+
+print(thislist[2:]) #includes from cherry to the lasts

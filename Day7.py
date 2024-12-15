70811cfc418037719c4f7f8a78fc4b73ec4a8a92
Hassnain khan <naintechnical1@gmail.com>
2024-12-15 18:49:41 +0500

    Day 7

---
+++ Day7.py
@@ -0,0 +1,10 @@
+# Day7
+# Change a Range of Item Values
+
+thislist = ["apple", "banana", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist1 = ["apple", "banana", "cherry"]
+thislist1[1:3] = ["watermelon"]
+print(thislist1)

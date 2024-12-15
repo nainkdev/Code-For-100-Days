4aa3011a80ca2fe3496ee5b8d510ddd06bc3e047
Hassnain khan <naintechnical1@gmail.com>
2024-12-15 18:49:18 +0500

    Day 6
    Sorry for the late upload there was a internet issue

---
+++ Day6.py
@@ -0,0 +1,8 @@
+#DAY 6
+#Check if Item Exists
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+
+
+if "apple" in thislist:
+      print("Yes, 'apple' is in the fruits list")
